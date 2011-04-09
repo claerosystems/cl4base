6056d296a0421ee29111165728a324b01a4555a2
Darryl Hein <dhein@xmmedia.com>
2011-04-08 20:09:57 -0600

    updating jQuery to 1.5.2

--- classes/controller/cl4/base.php
+++ classes/controller/cl4/base.php
@@ -253,7 +253,7 @@ class Controller_cl4_Base extends Controller_Template {
 
 		if (empty($this->template->scripts)) $this->template->scripts = array();
 		// add jquery js (for all pages, other js relies on it, so it has to be included first)
-		if ( ! isset($this->template->scripts['jquery'])) $this->template->scripts['jquery'] = '//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js';
+		if ( ! isset($this->template->scripts['jquery'])) $this->template->scripts['jquery'] = '//ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js';
 		if ( ! isset($this->template->scripts['jquery_ui'])) $this->template->scripts['jquery_ui'] = '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js';
 		if ( ! isset($this->template->scripts['cl4'])) $this->template->scripts['cl4'] = 'cl4/js/cl4.js';
 		if ( ! isset($this->template->scripts['cl4_ajax'])) $this->template->scripts['cl4_ajax'] = 'cl4/js/ajax.js';
