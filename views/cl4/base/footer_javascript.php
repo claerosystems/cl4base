8c6bccbda5e2f6f95ec2a2ca82a35250cac15d06
Darryl Hein <dhein@xmmedia.com>
2011-05-13 00:18:37 -0600

    adding jQuery "local" fall back

--- views/cl4/base/footer_javascript.php
+++ views/cl4/base/footer_javascript.php
@@ -1,4 +1,12 @@
 <?php
+// if jQuery is in the array of scripts, then include the path to jQuery and also a fallback to a local version
+if (isset($scripts['jquery'])) {
+	echo HTML::script($scripts['jquery']) . EOL; ?>
+<script>window.jQuery || document.write('<script src="/js/jquery.min.js">\x3C/script>')</script>
+<?php
+	unset($scripts['jquery']);
+} // if
+
 // Javascript, put all javascript here or in $on_load_js if possible
 foreach ($scripts as $file) echo HTML::script($file) . EOL;
 ?>
