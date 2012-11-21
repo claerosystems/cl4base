5daeac987938a69da99f8cbf5cff752ad34171fa
Darryl Hein <dhein@xmmedia.com>
2012-11-21 01:22:52 -0700

    simplifying the default menu in the admin

--- views/base/header.php
+++ views/base/header.php
@@ -4,30 +4,14 @@
 			<ul class="left">
 				<li class="home"><?php echo HTML::anchor('', __('Home')); ?></li>
 				<?php if ($logged_in) { ?>
-				<?php if (Auth::instance()->allowed('useradmin/index') || Auth::instance()->allowed('cl4admin') || (Auth::instance()->allowed('userguide') && Kohana::$environment == Kohana::DEVELOPMENT) || Auth::instance()->allowed('cl4admin/model_create') || Auth::instance()->allowed('dbchange/index')) { ?>
+				<?php if (Auth::instance()->allowed('cl4admin') || Auth::instance()->allowed('cl4admin/model_create')) { ?>
 				<li class="dbadmin has_subnav"><?php echo HTML::anchor(Route::get('cl4admin')->uri(), __('DB Admin') . '<span class="more"></span>'); ?>
 					<ul class="sub_nav">
-						<?php if (Auth::instance()->allowed('useradmin/index')) { ?>
-						<li class="useradmin"><?php echo HTML::anchor(Route::get('useradmin')->uri(), __('User Admin')); ?></li>
-						<?php } ?>
-						<?php if (Auth::instance()->allowed('useradmin/group/index')) { ?>
-						<li class="useradmin_groups"><?php echo HTML::anchor(Route::get('useradmin')->uri(array('action' => 'groups')), __('Groups/Permissions')); ?></li>
-						<?php } ?>
-						<?php if (Auth::instance()->allowed('contentadmin')) { ?>
-						<li class="content_admin"><?php echo HTML::anchor(Route::get('content_admin')->uri(), __('Content Admin')); ?></li>
-						<?php } ?>
 						<?php if (Auth::instance()->allowed('cl4admin')) { ?>
 						<li class="cl4admin"><?php echo HTML::anchor(Route::get('cl4admin')->uri(), __('DB Admin')); ?></li>
 						<?php } ?>
-						<?php if (Auth::instance()->allowed('userguide') && Kohana::$environment == Kohana::DEVELOPMENT) { ?>
-						<li class="userguide"><?php echo HTML::anchor(Route::get('docs/guide')->uri(), __('Kohana User Guide')); ?></li>
-						<li class="userguide_api"><?php echo HTML::anchor(Route::get('docs/api')->uri(), __('Kohana API Browser')); ?></li>
-						<?php } ?>
 						<?php if (CL4::is_dev() && Auth::instance()->allowed('cl4admin/model_create')) { ?>
 						<li class="modelcreate"><?php echo HTML::anchor(Route::get('cl4admin')->uri(array('model' => 'a', 'action' => 'model_create')), __('Model Create')); ?></li>
-						<?php } ?>
-						<?php if (Auth::instance()->allowed('dbchange/index')) { ?>
-						<li class="dbchange"><?php echo HTML::anchor(Route::get('dbchange')->uri(), __('DB Change')); ?></li>
 						<?php } ?>
 					</ul>
 				</li>
